dad59459726901906497ebdca1f404895ef0db3e
i7tsuki <tanaka_yi7@yahoo.co.jp>
2020-08-12 11:51:07 +0900

    実装完了

--- src/main.js
+++ src/main.js
@@ -1,8 +1,67 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    tasks: []
+  },
+  mutations: {
+    addTask(state, taskName) {
+      const newId = () => {
+        let maxId = 0;
+        state.tasks.forEach(element => { 
+          if (maxId < element.id) {
+            maxId = element.id;
+          }
+        })
+        maxId++;
+        return maxId;
+      };
+      state.tasks.push({
+        'id': newId(),
+        'taskName': taskName, 
+        'status': '作業中'
+      });
+    },
+    deleteTask(state, id) {
+      let i = 0;
+      state.tasks.some(element => { 
+        if (id === element.id) {
+          return true;
+        }
+        i++;
+      });
+      state.tasks.splice(i, 1);
+      store.commit('numbering');
+    },
+    numbering(state) {
+      let i = 1;
+      state.tasks.forEach(element => { 
+        element.id = i;
+        i++;
+      });
+    },
+    changeStatus(state, id) {
+      const foundTask = state.tasks.find(element => (id === element.id));
+      switch (foundTask.status) {
+        case '作業中':
+          foundTask.status = '完了';
+          break;
+        case '完了': 
+          foundTask.status = '作業中';
+          break;
+        default: 
+          console.log('該当しないtask.statusがあります');
+      }
+    }
+  }, 
+})
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App), 
 }).$mount('#app')
